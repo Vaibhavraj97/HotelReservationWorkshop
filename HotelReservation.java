f739a151a92a5ace4d0b7f102ca2084dd45e04ba
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 12:19:53 +0530

    Added:Ability to add special rates for reward customers as part of loyality

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,42 +1,75 @@
 package com.bridgelabz.HotelReservationSystem;
 
 
-	import java.time.LocalDate;
-	import java.util.ArrayList;
-	import java.util.Comparator;
-	import java.util.Optional;
+import java.util.ArrayList;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
-	public class HotelReservation implements HotelReservationII {
+public class HotelReservation implements HotelReservationII {
 
-	    ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
-	    Hotel hotel;
+    ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+    Hotel hotel ;
 
-	    public void addHotel(String hotelName, int rating, double regularCustomerRate) {
+    public void addHotel(String hotelName, int rating, double weekdayRate, double weekndRate) {
+        hotel = new Hotel();
+        hotel.setHotelName(hotelName);
+        hotel.setRating(rating);
+        hotel.setWeekDayRate(weekdayRate);
+        hotel.setWeekendRate(weekndRate);
+        hotelList.add(hotel);
+    }
 
-	        hotel = new Hotel();
-	        hotel.setHotelName(hotelName);
-	        hotel.setRating(rating);
-	        hotel.setRegularCustomerCost(regularCustomerRate);
-	        hotelList.add(hotel);
-	    }
+    public int getHotelListSize() {
+        return hotelList.size();
+    }
 
-	    public int getHotelListSize() {
-	        return hotelList.size();
-	    }
+    public void printHotelList() {
+        System.out.println(hotelList);
+    }
 
-	    public void printHotelList() {
-	        System.out.println(hotelList);
-	    }
+    public ArrayList<Hotel> getHotelList(){
+        return hotelList;
+    }
 
-	    public ArrayList<Hotel> getHotelList(){
-	        return hotelList;
-	    }
+    public String getCheapestHotel(LocalDate startDate, LocalDate endDate) {
 
-	    public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate) {
+        int numberOfDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        int weekends = 0;
 
-	        Optional<Hotel> resultList = hotelList.stream().min(Comparator.comparing(Hotel::getRegularCustomerCost));
-	        return resultList.get();
-	    }
+        while (startDate.compareTo(endDate) != 0) {
+            switch (DayOfWeek.of(startDate.get(ChronoField.DAY_OF_WEEK))) {
+                case SATURDAY:
+                    ++weekends;
+                    break;
+                case SUNDAY:
+                    ++weekends;
+                    break;
+            }
+            startDate = startDate.plusDays(1);
+        }
 
-	}
+        final int weekdaysNumber = numberOfDays - weekends;
+        final int weekendsNumber = weekends;
+
+        int cheapestRate = (int) (hotelList.get(0).getWeekDayRate() + hotelList.get(0).getWeekendRate());
+        String cheapestHotel=hotelList.get(0).getHotelName();
+        for (Hotel hotel : hotelList) {
+            int rateForHotel = (int) ((weekdaysNumber * hotel.getWeekDayRate())
+                    + (weekendsNumber * hotel.getWeekendRate()));
+            if (rateForHotel < cheapestRate) {
+                cheapestRate = rateForHotel;
+                cheapestHotel = hotel.getHotelName();
+            }
+        }
+
+
+        System.out.println("Cheapest Hotel : \n" + cheapestHotel + ", Total Rates: " + cheapestRate);
+        return cheapestHotel;
+
+    }
+
+}
 
