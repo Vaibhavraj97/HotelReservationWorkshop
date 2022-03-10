f4da3cc3f31213a03a18bc171e6178d98d35853d
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 12:27:39 +0530

    Added:Ability to add ratings to each hotel

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,52 +1,75 @@
 package com.bridgelabz.HotelReservationSystem;
 
-import java.time.LocalDate;
+
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Optional;
-import java.util.Scanner;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class HotelReservation implements HotelReservationII {
+
+    ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+    Hotel hotel ;
+
+    public void addHotel(String hotelName, int rating, double weekdayRate, double weekndRate) {
+        hotel = new Hotel();
+        hotel.setHotelName(hotelName);
+        hotel.setRating(rating);
+        hotel.setWeekDayRate(weekdayRate);
+        hotel.setWeekendRate(weekndRate);
+        hotelList.add(hotel);
+    }
+
+    public int getHotelListSize() {
+        return hotelList.size();
+    }
+
+    public void printHotelList() {
+        System.out.println(hotelList);
+    }
+
+    public ArrayList<Hotel> getHotelList(){
+        return hotelList;
+    }
+
+    public String getCheapestHotel(LocalDate startDate, LocalDate endDate) {
+
+        int numberOfDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        int weekends = 0;
+
+        while (startDate.compareTo(endDate) != 0) {
+            switch (DayOfWeek.of(startDate.get(ChronoField.DAY_OF_WEEK))) {
+                case SATURDAY:
+                    ++weekends;
+                    break;
+                case SUNDAY:
+                    ++weekends;
+                    break;
+            }
+            startDate = startDate.plusDays(1);
+        }
+
+        final int weekdaysNumber = numberOfDays - weekends;
+        final int weekendsNumber = weekends;
+
+        int cheapestRate = (int) (hotelList.get(0).getWeekDayRate() + hotelList.get(0).getWeekendRate());
+        String cheapestHotel=hotelList.get(0).getHotelName();
+        for (Hotel hotel : hotelList) {
+            int rateForHotel = (int) ((weekdaysNumber * hotel.getWeekDayRate())
+                    + (weekendsNumber * hotel.getWeekendRate()));
+            if (rateForHotel < cheapestRate) {
+                cheapestRate = rateForHotel;
+                cheapestHotel = hotel.getHotelName();
+            }
+        }
 
 
-	    public class HotelReservation implements HotelReservationII {
-	        String hotelName;
-	        int rate;
-	        double regularCustomerRate;
-	        Scanner sc = new Scanner(System.in);
-	        ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
-	        Hotel hotel;
-	        public void addHotel() {
-	            System.out.println("How many hotels you want to add");
-	            int numOfHotels = sc.nextInt();
-	            while(numOfHotels > 0) {
-	                System.out.println("Enter hotel name:");
-	                hotelName = sc.next();
-	                System.out.println("Enter hotel rate:");
-	                rate = sc.nextInt();
-	                System.out.println("Enter regular customer rate:");
-	                regularCustomerRate = sc.nextDouble();
-	                hotel = new Hotel(hotelName, rate,regularCustomerRate);
-	                hotelList.add(hotel);
-	                numOfHotels--;
-	            }
-	        }
-	        public int getHotelListSize() {
-	            return hotelList.size();
-	        }
+        System.out.println("Cheapest Hotel : \n" + cheapestHotel + ", Total Rates: " + cheapestRate);
+        return cheapestHotel;
 
-	        public void printHotelList() {
-	            System.out.println(hotelList);
-	        }
-	        public ArrayList<Hotel> getHotelList(){
-	            return hotelList;
-	        }
+    }
 
-	        public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate) {
+}
 
-	            Optional<Hotel> resultList = hotelList.stream().min(Comparator.comparing(Hotel::getRegularCustomerCost));
-	            return resultList.get();
-	        }
-
-	    }
-
-	    
-	
