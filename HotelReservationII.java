6b69808a230988d121c23c16f48f6267dafe9062
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 12:42:17 +0530

    Added:Ability to find chepest price hotel for given date range

---
+++ HotelReservationII.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.HotelReservationSystem;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public interface HotelReservationII {
+
+     public void addHotel(String hotelName,int rating, double weekDayRate,double weekendRate);
+     public int getHotelListSize();
+     public void printHotelList();
+     public ArrayList<Hotel> getHotelList();
+     public String getCheapestHotel(LocalDate startDate, LocalDate endDate);
+
+}
