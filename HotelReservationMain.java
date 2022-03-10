17bcb1996d62ee07b707281d6f78dad9ef4f3cd6
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 12:28:16 +0530

    Added:Ability to add ratings to each hotel

--- HotelReservationMain.java
+++ HotelReservationMain.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.HotelReservationSystem;
 
+import java.time.LocalDate;
+import java.time.Month;
 
-	public class HotelReservationMain {
-	    public static void main(String[] args) {
-	        System.out.println(" Welcome To Hotel Reservation");
-	        HotelReservationII hotelReservation = new HotelReservation();
-	        hotelReservation.addHotel();
-	        hotelReservation.printHotelList();
-	    }
-	}
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println(" Welcome To Hotel Reservation");
+        HotelReservationII hotelReservation = new HotelReservation();
+        hotelReservation.addHotel("Lakewood", 3, 110,90);
+        hotelReservation.addHotel("Bridgewood", 4, 150,50);
+        hotelReservation.addHotel("Ridgewood", 5, 220,150);
+        hotelReservation.printHotelList();
+        LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);
+        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
+        hotelReservation.getCheapestHotel(startDate, endDate);
+    }
+}
