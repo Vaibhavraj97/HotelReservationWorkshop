1099201eb79ac15c0eb1d6272aabb8afd0febe01
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 13:04:35 +0530

    Added:Ability to find best rated hotel

---
+++ HotelReservationII.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.HotelReservationSystem;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public interface HotelReservationII {
+
+     public void addHotel(String hotelName,int rating, double weekDayRate,double weekendRate,double weekdayRewardCustomerRate,double weekendRewardCustomerRate);
+     public int getHotelListSize();
+     public void printHotelList();
+     public ArrayList<Hotel> getHotelList();
+     public String getCheapestHotel(LocalDate startDate, LocalDate endDate);
+     public String getBestRatedHotel(LocalDate startDate, LocalDate endDate);
+
+}
