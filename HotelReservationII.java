92f461766d860dcd300f80d189d25ea190e5f9ca
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 12:20:27 +0530

    Added:Ability to add weekday and weekend rates for each hotel

--- HotelReservationII.java
+++ HotelReservationII.java
@@ -2,12 +2,15 @@ package com.bridgelabz.HotelReservationSystem;
 
 
 	import java.util.ArrayList;
+	import java.time.LocalDate;
+	import java.util.ArrayList;
 
 	public interface HotelReservationII {
 
-	    public void addHotel(String hotelName,int rating, double regularCustomerRate);
-	    public int getHotelListSize();
-	    public void printHotelList();
-	    public ArrayList<Hotel> getHotelList();
+	     void addHotel(String hotelName,int rating, double weekDayRate,double weekendRate);
+	     int getHotelListSize();
+	     void printHotelList();
+	     ArrayList<Hotel> getHotelList();
+	     String getCheapestHotel(LocalDate startDate, LocalDate endDate);
 	}
-
+	
