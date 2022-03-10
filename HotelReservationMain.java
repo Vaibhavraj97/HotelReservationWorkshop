58605272b6bd832c462a8401c2c2a60c4fe4e63d
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 11:36:43 +0530

    Added:Ability to find cheapest best rated hotel fot given date

---
+++ HotelReservationMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.HotelReservationSystem;
+
+
+	public class HotelReservationMain {
+	    public static void main(String[] args) {
+	        System.out.println(" Welcome To Hotel Reservation");
+	        HotelReservationII hotelReservation = new HotelReservation();
+	        hotelReservation.addHotel();
+	        hotelReservation.printHotelList();
+	    }
+	}
