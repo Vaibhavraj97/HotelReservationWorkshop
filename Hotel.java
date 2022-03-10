2ce93ea1ecc4559fa5cf77f4f0fe096fefad70fd
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-10 12:03:28 +0530

    Added:Ability to add weekday and weekend rates for each hotel

---
+++ Hotel.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.HotelReservationSystem;
+	public class Hotel {
+		 private String hotelName;
+		    private double weekDayRate;
+		    private double weekendRate;
+		    public Hotel() {
+		    }
+
+		    public Hotel(String hotelName, int rate, double weekDayRate, double weekendRate) {
+		        this.hotelName = hotelName;
+		        this.weekDayRate = weekDayRate;
+		        this.weekendRate=weekendRate;
+		    }
+
+		    public String getHotelName() {
+		        return hotelName;
+		    }
+
+		    public void setHotelName(String hotelName) {
+		        this.hotelName = hotelName;
+		    }
+
+		    public double getWeekDayRate() {
+		        return weekDayRate;
+		    }
+
+		    public void setWeekDayRate(double weekDayRate) {
+		        this.weekDayRate = weekDayRate;
+		    }
+
+		    public double getWeekendRate() {
+		        return weekendRate;
+		    }
+
+		    public void setWeekendRate(double weekendRate) {
+		        this.weekendRate = weekendRate;
+		    }
+
+		    @Override
+		    public String toString() {
+		        return "Hotel [hotelName=" + hotelName + ", weekDayRate=" + weekDayRate+ ", weekendRate=" + weekendRate + "]";
+		    }
+
+
+
+		}
+	
+
+
+	   
